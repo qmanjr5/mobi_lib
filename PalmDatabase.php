92d0fe9b7cac44d998f6df64abca87210355c0f5
qmanjr5 <qmanjr5@tigers.dreamhost.com>
2012-09-25 20:57:21 -0700

    Initial commit

---
+++ PalmDatabase.php
@@ -0,0 +1,27 @@
+<?php
+class PalmDatabase
+{
+	protected $filehandle;
+	public function __construct($file)
+	{
+		if(is_string($file))
+		{
+			$this->$filehandle = fopen($file);
+		}
+		elseif(is_resource($file))
+		{
+			$this->$filehandle = $file;
+		}
+		else
+		{
+			return false;
+		}
+
+		$this->load();
+	}
+	public function load();
+	{
+		$this->properties = new PalmDatabase_Properties($this->$filehandle);
+		$this->records = new PalmDatabase_Records($this->$filehandle);
+	}
+}
