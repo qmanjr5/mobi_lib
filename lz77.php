7714dfcf47321918257201ba9e296b62c6e96740
Quentin B <quentin@quentinboughner.com>
2012-11-21 22:38:24 -0500

    :(

--- lz77.php
+++ lz77.php
@@ -13,11 +13,12 @@ class PalmDoc_LZ77
 		if(is_string($data))
 		{
 			$fp = fopen("data://text/plain;base64," . base64_encode($data), "r");
-			while($val = fread($fp, 1))
+			while(!feof($fp))
 			{
+				echo $val;
+				$val = fread($fp, 1);
 				if($val == 0x00 || ($val >= 0x09 && $val <=0x7F))
 				{
-					echo $val . "\n";
 					$decompressed .= $val;
 				}
 				elseif($val>=0x01 && $val<=0x08)
@@ -26,17 +27,20 @@ class PalmDoc_LZ77
 				}
 				elseif($val>=0x80 && $val<=0xbf)
 				{
-					echo "Hello, world." . "\n";
+					echo "Length, distance\n";	
 					$val .= fread($fp, 1);
 					$pair_distance = 0x422d & $val;
-					echo "Pair: Distance: " . $pair_distance . "\n";
 					$pair_length = 0x07 & $val;
-					echo "Pair: Length: " . $pair_length . "\n";
 					$decompressed .= substr($decompressed, strlen($decompressed)-$pair_distance, $pair_length);
 				}
 				elseif($val>=0xc0 && $val<=0xff)
 				{
+					echo "4th if\n";
 					$decompressed .= " " . ($val ^ 0x80);
+				}
+				else
+				{
+					echo "Else\n";
 				}
 			}
 			return $decompressed;
